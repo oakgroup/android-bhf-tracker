ed3d68ae3a5436505413dccb0550364b5d87f4ff
Omar Brugna <omar@activeorbit.co.uk>
2023-06-06 15:33:59 +0200

    Migration from CoroutineWorker to ListenableWorker to execute async work

--- src/main/java/com/active/orbit/tracker/core/upload/TrackerUploadWorker.kt
+++ src/main/java/com/active/orbit/tracker/core/upload/TrackerUploadWorker.kt
@@ -1,59 +1,132 @@
 package com.active.orbit.tracker.core.upload
 
 import android.content.Context
-import androidx.work.CoroutineWorker
+import androidx.concurrent.futures.CallbackToFutureAdapter
 import androidx.work.ForegroundInfo
+import androidx.work.ListenableWorker
 import androidx.work.WorkerParameters
 import com.active.orbit.tracker.R
+import com.active.orbit.tracker.core.listeners.ResultListener
 import com.active.orbit.tracker.core.preferences.engine.TrackerPreferences
 import com.active.orbit.tracker.core.tracker.TrackerNotification
 import com.active.orbit.tracker.core.upload.uploaders.*
 import com.active.orbit.tracker.core.utils.Logger
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+import com.google.common.util.concurrent.ListenableFuture
 
-class TrackerUploadWorker(val context: Context, workerParams: WorkerParameters) : CoroutineWorker(context, workerParams) {
+
+class TrackerUploadWorker(val context: Context, workerParams: WorkerParameters) : ListenableWorker(context, workerParams) {
 
     companion object {
         var sendingData: Boolean = false
         private const val NOTIFICATION_ID = 9973
     }
 
-    override suspend fun doWork(): Result {
-        return try {
-            withContext(Dispatchers.IO) {
+    override fun startWork(): ListenableFuture<Result> {
+        return CallbackToFutureAdapter.getFuture { completer ->
+
+            try {
                 Logger.d("Data Upload Work manager fired")
-                uploadData()
-                Result.success()
+                uploadData(object : ResultListener {
+                    override fun onResult(success: Boolean) {
+                        sendingData = false
+                        completer.set(Result.success())
+                    }
+                })
+            } catch (e: Throwable) {
+                completer.set(Result.failure())
             }
-        } catch (e: Throwable) {
-            Result.failure()
         }
     }
 
-    private fun uploadData() {
-        if (sendingData) return
+    private fun uploadData(listener: ResultListener? = null) {
+        if (sendingData) {
+            listener?.onResult(true)
+            return
+        }
+
         if (TrackerPreferences.backend(context).uploadData) {
             sendingData = true
             if (!TrackerPreferences.user(context).isUserRegistered()) {
                 Logger.d("No user Id assigned yet")
+                listener?.onResult(false)
                 return
             }
+
+            var callbacks = 0
+            val callbacksExpected = 6
+
             // upload data
-            if (TrackerPreferences.config(context).useActivityRecognition) ActivitiesUploader.uploadData(context)
-            if (TrackerPreferences.config(context).useBatteryMonitor) BatteriesUploader.uploadData(context)
-            if (TrackerPreferences.config(context).useLocationTracking) LocationsUploader.uploadData(context)
-            if (TrackerPreferences.config(context).useStepCounter) StepsUploader.uploadData(context)
-            if (TrackerPreferences.config(context).useHeartRateMonitor) HeartRatesUploader.uploadData(context)
-            if (TrackerPreferences.config(context).useMobilityModelling) TripsUploader.uploadData(context)
-            sendingData = false
+            if (TrackerPreferences.config(context).useActivityRecognition) {
+                ActivitiesUploader.uploadData(context, object : ResultListener {
+                    override fun onResult(success: Boolean) {
+                        callbacks++
+                        if (callbacks == callbacksExpected) {
+                            listener?.onResult(true)
+                        }
+                    }
+                })
+            }
+            if (TrackerPreferences.config(context).useBatteryMonitor) {
+                BatteriesUploader.uploadData(context, object : ResultListener {
+                    override fun onResult(success: Boolean) {
+                        callbacks++
+                        if (callbacks == callbacksExpected) {
+                            listener?.onResult(true)
+                        }
+                    }
+                })
+            }
+            if (TrackerPreferences.config(context).useLocationTracking) {
+                LocationsUploader.uploadData(context, object : ResultListener {
+                    override fun onResult(success: Boolean) {
+                        callbacks++
+                        if (callbacks == callbacksExpected) {
+                            listener?.onResult(true)
+                        }
+                    }
+                })
+            }
+            if (TrackerPreferences.config(context).useStepCounter) {
+                StepsUploader.uploadData(context, object : ResultListener {
+                    override fun onResult(success: Boolean) {
+                        callbacks++
+                        if (callbacks == callbacksExpected) {
+                            listener?.onResult(true)
+                        }
+                    }
+                })
+            }
+            if (TrackerPreferences.config(context).useHeartRateMonitor) {
+                HeartRatesUploader.uploadData(context, object : ResultListener {
+                    override fun onResult(success: Boolean) {
+                        callbacks++
+                        if (callbacks == callbacksExpected) {
+                            listener?.onResult(true)
+                        }
+                    }
+                })
+            }
+            if (TrackerPreferences.config(context).useMobilityModelling) {
+                TripsUploader.uploadData(context, object : ResultListener {
+                    override fun onResult(success: Boolean) {
+                        callbacks++
+                        if (callbacks == callbacksExpected) {
+                            listener?.onResult(true)
+                        }
+                    }
+                })
+            }
+        } else {
+            listener?.onResult(true)
         }
     }
 
-    override suspend fun getForegroundInfo(): ForegroundInfo {
-        TrackerNotification.notificationText = "Do not close the app, please"
-        TrackerNotification.notificationIcon = R.drawable.ic_notification
-        val notification = TrackerNotification(context, NOTIFICATION_ID, true)
-        return ForegroundInfo(NOTIFICATION_ID, notification.notification!!)
+    override fun getForegroundInfoAsync(): ListenableFuture<ForegroundInfo> {
+        return CallbackToFutureAdapter.getFuture { completer ->
+            TrackerNotification.notificationText = "Do not close the app, please"
+            TrackerNotification.notificationIcon = R.drawable.ic_notification
+            val notification = TrackerNotification(context, NOTIFICATION_ID, true)
+            completer.set(ForegroundInfo(NOTIFICATION_ID, notification.notification!!))
+        }
     }
 }
