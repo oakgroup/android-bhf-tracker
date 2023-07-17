c6fcc647c12ff05434c0e074a00623c642d31854
Omar Brugna <omar@activeorbit.co.uk>
2023-07-17 18:34:13 +0200

    Removed unused broadcast directory

--- src/main/java/com/active/orbit/tracker/core/broadcast/TrackerBroadcastListener.kt
+++
@@ -1,8 +0,0 @@
-package com.active.orbit.tracker.core.broadcast
-
-import android.content.Context
-
-interface TrackerBroadcastListener {
-
-    fun onBroadcast(context: Context, @TrackerBroadcastType type: String, sentByMyself: Boolean, identifier: String, subIdentifier: String)
-}
