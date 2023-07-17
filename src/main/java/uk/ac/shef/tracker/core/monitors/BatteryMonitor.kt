94a10778df78d558e988b487e65495c8dbe459f6
Omar Brugna <omar@activeorbit.co.uk>
2023-07-17 18:52:54 +0200

    Updated copyright

--- src/main/java/uk/ac/shef/tracker/core/monitors/BatteryMonitor.kt
+++ src/main/java/uk/ac/shef/tracker/core/monitors/BatteryMonitor.kt
@@ -1,3 +1,7 @@
+/*
+ *  Copyright (c) 2023. This code has been developed by Fabio Ciravegna, The University of Sheffield. All rights reserved. No part of this code can be used without the explicit written permission by the author
+ */
+
 package uk.ac.shef.tracker.core.monitors
 
 import uk.ac.shef.tracker.core.database.models.TrackerDBBattery
