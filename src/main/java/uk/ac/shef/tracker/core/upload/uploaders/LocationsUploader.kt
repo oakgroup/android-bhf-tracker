2ac8c1dfee9f043d459bc07aab3c300416758572
Fabio Ciravegna <f.ciravegna@sheffield.ac.uk>
2023-08-03 10:53:49 +0100

    xml files

--- src/main/java/uk/ac/shef/tracker/core/upload/uploaders/LocationsUploader.kt
+++ src/main/java/uk/ac/shef/tracker/core/upload/uploaders/LocationsUploader.kt
@@ -1,5 +1,8 @@
 /*
- *  Copyright (c) 2023. This code has been developed by Fabio Ciravegna, The University of Sheffield. All rights reserved. No part of this code can be used without the explicit written permission by the author
+ *   Copyright (c) 2023. 
+ *   This code was developed by Fabio Ciravegna, The University of Sheffield. 
+ *   All rights reserved. 
+ *   No part of this code can be used without the explicit written permission by the author
  */
 
 package uk.ac.shef.tracker.core.upload.uploaders
